bd182a5fc1f25913ccd02f8ce8135a7191bc40da
Karoly Lonich <karoly.lonich@smartcoder.dev>
2022-12-08 22:34:56 +0100

    feat: Add API routes and their functions

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-const { group } = require("console");
-const express = require("express"),
-  morgan = require("morgan"),
-  bodyParser = require("body-parser"),
-  uuid = require("uuid");
-(fs = require("fs")), (path = require("path"));
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const uuid = require("uuid");
+const fs = require("fs");
+const path = require("path");
 
-//Const
+// ===============================================================================
+// === Const
+// ===============================================================================
 
 const PORT = 3000;
 const HOST = "0.0.0.0";
 
-let users = [];
+let users = [
+  {
+    id: "1",
+    userName: "kal",
+    name: "Karoly Lonich",
+    favoriteMovies: [],
+  },
+  {
+    id: "2",
+    userName: "john",
+    name: "John Doe",
+    favoriteMovies: [],
+  },
+];
 
 let movies = [
   {
@@ -99,13 +114,20 @@ let movies = [
       "https://artworks.thetvdb.com/banners/v4/movie/36635/posters/631ccb33d104f.jpg",
   },
 ];
-//App
+
+// ===============================================================================
+// === App
+// ===============================================================================
 
 const app = express();
 
 const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
   flags: "a",
 });
+
+// ===============================================================================
+// === Middlewares
+// ===============================================================================
 
 app.use(morgan("combined", { stream: accessLogStream }));
 
@@ -117,6 +139,39 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
 });
+
+// ===============================================================================
+// === CREATE
+// ===============================================================================
+
+app.post("/users", (req, res) => {
+  const newUser = req.body;
+
+  if (newUser.userName) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser);
+  } else {
+    res.status(400).send("Users shall have a valid username!");
+  }
+});
+
+app.post("/users/:id/:movieTitle", (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    user.favoriteMovies.push(movieTitle);
+    res.status(200).send(`${movieTitle} was added to the user ${id}'s array`);
+  } else {
+    res.status(400).send("User was not found.");
+  }
+});
+
+// ===============================================================================
+// === READ
+// ===============================================================================
 
 app.get("/movies", (req, res) => {
   res.status(200).json(movies);
@@ -134,7 +189,7 @@ app.get("/movies/:title", (req, res) => {
 
 app.get("/movies/genres/:genreName", (req, res) => {
   const { genreName } = req.params;
-  let genre = movies.find((movie) => movie.genre.name === genreName);
+  let genre = movies.find((movie) => movie.genre.name === genreName).genre;
   if (genre) {
     return res.status(200).json(genre);
   } else {
@@ -144,13 +199,82 @@ app.get("/movies/genres/:genreName", (req, res) => {
 
 app.get("/movies/directors/:directorName", (req, res) => {
   const { directorName } = req.params;
-  let director = movies.find((movie) => movie.director.name === directorName);
+  let director = movies.find(
+    (movie) => movie.director.name === directorName
+  ).director;
   if (director) {
     return res.status(200).json(director);
   } else {
     return res.status(400).send("Director not found");
   }
 });
+
+// ===============================================================================
+// === UPDATE
+// ===============================================================================
+
+app.put("/users/:id/:userName", (req, res) => {
+  const { id, userName } = req.params;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    user.userName = userName;
+    res.status(200).json(user);
+  } else {
+    res.status(400).send("User was not found.");
+  }
+});
+
+// ===============================================================================
+// === DELETE
+// ===============================================================================
+
+app.delete("/users/:id/:movieTitle", (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    user.favoriteMovies = user.favoriteMovies.filter(
+      (title) => title != movieTitle
+    );
+    res
+      .status(200)
+      .send(`${movieTitle} was deleted from the user ${id}'s array`);
+  } else {
+    res.status(400).send("User was not found.");
+  }
+});
+
+app.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    users = users.filter((user) => user.id != id);
+    res
+      .status(200)
+      .send(
+        `Username: ${user.userName} with ID: ${id} was deleted from the users array`
+      );
+  } else {
+    res.status(400).send("User was not found.");
+  }
+});
+
+// ===============================================================================
+// === Static
+// ===============================================================================
+
+app.get("/documentation", (req, res) => {
+  res.status(200).sendFile("public/index.html", { root: __dirname });
+});
+
+// ===============================================================================
+// === Server
+// ===============================================================================
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
--- public/doc.js
+++ public/doc.js
@@ -23,6 +23,30 @@
  */
 
 /**
+ * @apiDefine DirectorNotFoundError
+ *
+ * @apiError (Error / Status Code: 404) DirectorNotFound The name of the <code>directorName</code> was not found.
+ *
+ * @apiErrorExample {json} Error-Response:
+ * HTTP/1.1 404 Not Found
+ * {
+ *   "error": "DirectorNotFound"
+ * }
+ */
+
+/**
+ * @apiDefine UserNotFoundError
+ *
+ * @apiError UserNotFound The id of the User was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "UserNotFound"
+ *     }
+ */
+
+/**
  * @api {get} /movies Request the list of all movies
  * @apiName GetMovies
  * @apiGroup Movie
@@ -38,36 +62,32 @@
  * HTTP/1.1 200 OK
  *[
     {
-      title: "Black Panther: Wakanda Forever",
-      description:
-        "Queen Ramonda, Shuri, M’Baku, Okoye and the Dora Milaje fight to protect their nation from intervening world powers in the wake of King T’Challa’s death. As the Wakandans strive to embrace their next chapter, the heroes must band together with the help of War Dog Nakia and Everett Ross and forge a new path for the kingdom of Wakanda.",
-      genre: {
-        name: "Action",
-        description: "",
-      },
-      director: {
-        name: "Ryan Coogler",
-        bio: "Ryan Kyle Coogler is an American film director, producer and screenwriter. He is a recipient of four NAACP Image Awards, four Black Reel Awards and an Academy Award nomination for Best Picture.",
-        birth: "1986",
-      },
-      imageURL:
-        "https://artworks.thetvdb.com/banners/v4/movie/31110/backgrounds/636fafaf50adc.jpg",
+        "title": "Black Panther: Wakanda Forever",
+        "description": "Queen Ramonda, Shuri, M’Baku, Okoye and the Dora Milaje fight to protect their nation from intervening world powers in the wake of King T’Challa’s death. As the Wakandans strive to embrace their next chapter, the heroes must band together with the help of War Dog Nakia and Everett Ross and forge a new path for the kingdom of Wakanda.",
+        "genre": {
+            "name": "Action",
+            "description": "Action film is a film genre in which the protagonist is thrust into a series of events that typically involve violence and physical feats."
+        },
+        "director": {
+            "name": "Ryan Coogler",
+            "bio": "Ryan Kyle Coogler is an American film director, producer and screenwriter. He is a recipient of four NAACP Image Awards, four Black Reel Awards and an Academy Award nomination for Best Picture.",
+            "birth": "1986"
+        },
+        "imageURL": "https://artworks.thetvdb.com/banners/v4/movie/31110/backgrounds/636fafaf50adc.jpg"
     },
     {
-      title: "The Menu",
-      description:
-        "A couple travels to a coastal island to eat at an exclusive restaurant where the chef has prepared a lavish menu, with some shocking surprises.",
-      genre: {
-        name: "Horror",
-        description: "",
-      },
-      director: {
-        name: "Mark Mylod",
-        bio: "Mark Mylod is a British television and film director and executive producer. He is known for his work on the television series Succession and Shameless.",
-        birth: "1965",
-      },
-      imageURL:
-        "https://artworks.thetvdb.com/banners/v4/movie/337249/posters/632289ce2fc4d.jpg",
+        "title": "The Menu",
+        "description": "A couple travels to a coastal island to eat at an exclusive restaurant where the chef has prepared a lavish menu, with some shocking surprises.",
+        "genre": {
+            "name": "Horror",
+            "description": "Horror is a film genre that seeks to elicit fear or disgust in its audience for entertainment purposes. Horror films often explore dark subject matter and may deal with transgressive topics or themes. Broad elements include monsters, apocalyptic events, and religious or folk beliefs."
+        },
+        "director": {
+            "name": "Mark Mylod",
+            "bio": "Mark Mylod is a British television and film director and executive producer. He is known for his work on the television series Succession and Shameless.",
+            "birth": "1965"
+        },
+        "imageURL": "https://artworks.thetvdb.com/banners/v4/movie/337249/posters/632289ce2fc4d.jpg"
     }
   ];
  */
@@ -89,21 +109,19 @@
  * @apiSuccessExample Success-Response:
  * HTTP/1.1 200 OK
  * {
-      title: "Black Panther: Wakanda Forever",
-      description:
-        "Queen Ramonda, Shuri, M’Baku, Okoye and the Dora Milaje fight to protect their nation from intervening world powers in the wake of King T’Challa’s death. As the Wakandans strive to embrace their next chapter, the heroes must band together with the help of War Dog Nakia and Everett Ross and forge a new path for the kingdom of Wakanda.",
-      genre: {
-        name: "Action",
-        description: "",
-      },
-      director: {
-        name: "Ryan Coogler",
-        bio: "Ryan Kyle Coogler is an American film director, producer and screenwriter. He is a recipient of four NAACP Image Awards, four Black Reel Awards and an Academy Award nomination for Best Picture.",
-        birth: "1986",
-      },
-      imageURL:
-        "https://artworks.thetvdb.com/banners/v4/movie/31110/backgrounds/636fafaf50adc.jpg",
-   }
+    "title": "Black Panther: Wakanda Forever",
+    "description": "Queen Ramonda, Shuri, M’Baku, Okoye and the Dora Milaje fight to protect their nation from intervening world powers in the wake of King T’Challa’s death. As the Wakandans strive to embrace their next chapter, the heroes must band together with the help of War Dog Nakia and Everett Ross and forge a new path for the kingdom of Wakanda.",
+    "genre": {
+        "name": "Action",
+        "description": "Action film is a film genre in which the protagonist is thrust into a series of events that typically involve violence and physical feats."
+    },
+    "director": {
+        "name": "Ryan Coogler",
+        "bio": "Ryan Kyle Coogler is an American film director, producer and screenwriter. He is a recipient of four NAACP Image Awards, four Black Reel Awards and an Academy Award nomination for Best Picture.",
+        "birth": "1986"
+    },
+    "imageURL": "https://artworks.thetvdb.com/banners/v4/movie/31110/backgrounds/636fafaf50adc.jpg"
+  }
  *
  * @apiUse MovieNotFoundError
  */
@@ -111,7 +129,7 @@
 /**
  * @api {get} /movies/genres/:genreName Request genre information
  * @apiName GetMovieGenre
- * @apiGroup Genre
+ * @apiGroup Movie
  *
  * @apiParam {String} genreName Genre unique name.
  *
@@ -122,10 +140,9 @@
  * @apiSuccessExample {json} Success-Response:
  *HTTP/1.1 200 OK
  *{
- *   name: "Action",
- *   description:
- *     "Action film is a film genre in which the protagonist is thrust into a series of events that typically involve violence and physical feats.*",
- *}
+    "name": "Adventure",
+    "description": "Adventure fiction is a type of fiction that usually presents danger, or gives the reader a sense of excitement. Some adventure fiction also satisfies the literary definition of romance fiction."
+  }
  *
  * @apiUse GenreNotFoundError
  */
@@ -133,7 +150,7 @@
 /**
  * @api {get} /movies/directors/:directorName Request director information
  * @apiName GetMoviedirector
- * @apiGroup Director
+ * @apiGroup Movie
  *
  * @apiParam {String} directorName Director unique name.
  *
@@ -145,10 +162,108 @@
  * @apiSuccessExample {json} Success-Response:
  *HTTP/1.1 200 OK
  *{
-      name: "Ryan Coogler",
-      bio: "Ryan Kyle Coogler is an American film director, producer and screenwriter. He is a recipient of four NAACP Image Awards, four Black Reel Awards and an Academy Award nomination for Best Picture.",
-      birth: "1986",
+    "name": "Ryan Coogler",
+    "bio": "Ryan Kyle Coogler is an American film director, producer and screenwriter. He is a recipient of four NAACP Image Awards, four Black Reel Awards and an Academy Award nomination for Best Picture.",
+    "birth": "1986"
   }
  *
  * @apiUse DirectorNotFoundError
+ */
+
+/**
+ * @api {post} /users/ Create new user
+ * @apiName CreateUser
+ * @apiGroup User
+ *
+ * @apiBody {String} userName="johndoe"       Mandatory username
+ * @apiBody {String} [name]           Optional name
+ * @apiBody {String} [country="US"]      Optional with default value "US".
+ * @apiBody {Number} [age=18]          Optional Age with default 18.
+ * @apiBody (Login) {String} [pass]      User password.
+ * @apiBody {Object} [address]         Optional nested address object.
+ * @apiBody {String} [address[street]] Optional street and number.
+ * @apiBody {String} [address[zip]]    Optional zip code.
+ * @apiBody {String} [address[city]]   Optional city.
+ *
+ * @apiSuccess (Success / Status Code: 200) {String} userName Username of the User.
+ *
+ * @apiSuccessExample  {json} Success-Response:
+HTTP/1.1 201 OK
+ {
+    "userName": "kal",
+    "name": "",
+    "favoriteMovies": [],
+    "country": "US",
+    "age": 18,
+    "pass": "",
+    "address[street]": "",
+    "address[zip]": "",
+    "address[city]": "",
+    "id": "8b3bdaf7-b3e9-46da-a29f-98256f64e3f8"
+  }
+ *
+ *
+ */
+
+/**
+ * @api {put} /users/:id/:userName Update username
+ * @apiName UpdateUserName
+ * @apiGroup User
+ *
+ * @apiParam {Number} id          Users unique ID.
+ * @apiParam {String} userName  Username of the User.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *  {
+        "id": "1",
+        "userName": "kal",
+        "name": "Karoly Lonich",
+        "favoriteMovies": []
+    }
+ *
+ * @apiUse UserNotFoundError
+ */
+
+/**
+ * @api {post} /users/:id/:movieTitle Add movie title to user's favorite movie list.
+ * @apiName AddUserFavoriteMovie
+ * @apiGroup User
+ *
+ * @apiParam {Number} id          Users unique ID.
+ * @apiParam {String} movieTitle  The title of the movie to be added to the user's favorite list.
+ *
+ * @apiSuccessExample {String} Success-Response:
+ *    HTTP/1.1 200 OK
+ *      Casablanca was added to the user 1's array
+ *
+ * @apiUse UserNotFoundError
+ */
+
+/**
+ * @api {delete} /users/:id/:movieTitle Delete movie title from user's favorite movie list.
+ * @apiName DeleteUserFavoriteMovie
+ * @apiGroup User
+ *
+ * @apiParam {Number} id          Users unique ID.
+ * @apiParam {String} movieTitle  The title of the movie to be added to the user's favorite list.
+ *
+ * @apiSuccessExample {String} Success-Response:
+ *     HTTP/1.1 200 OK
+ *      Casablanca was deleted from the user 1's array
+ * @apiUse UserNotFoundError
+ */
+
+/**
+ * @api {delete} /users/:id Delete user
+ * @apiName DeleteUser
+ * @apiGroup User
+ *
+ * @apiParam {Number} id          User unique ID.
+ *
+ * @apiSuccessExample {String} Success-Response:
+ *    HTTP/1.1 200 OK
+ *      Username: kal with ID: 1 was deleted from the users array
+
+ * @apiUse UserNotFoundError
  */

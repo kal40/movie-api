8d6df07f34b652a230ca1309e733cdfcc981e735
Karoly Lonich <karoly.lonich@smartcoder.dev>
2023-01-24 21:58:09 +0100

    fix: update openapi definition url

--- public/swagger-initializer.js
+++ public/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function () {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: `https://myflixapi.smartcoder.dev/openapi`,
+    url: `https://myflixapi.smartcoder.dev/openapi.yml`,
     // url: "http://localhost:3000/openapi",
     dom_id: "#swagger-ui",
     deepLinking: true,
